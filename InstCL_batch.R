2a1efc992745b5bc0b5d4067a89cfe2a6c6acc0b
Vince Darcangelo <160167113+VinnyD33@users.noreply.github.com>
2025-07-18 14:29:08 -0600

    Update InstCL_batch.R
    Updated code and formatted

--- InstCL_batch.R
+++ InstCL_batch.R
@@ -1,18 +1,17 @@
-#########################################################################
-# Log of instructor info/class assgn since fall 2017
-# L:\mgt\FCQ\R_Code\campus_labs\instCL_batch.R - Vince Darcangelo, 08/04/22
-# Instructions
-# Update # TERM UPDATE before each run - 
-# Copy/paste to # PAST SEMESTERS after
-# The purpose of this code is to maintain a running log of all evaluated instrs for reference and name/constituent id change purposes
-#########################################################################
+##########################################################################
+# Maintain running log of all evaluated instructors since fall 2017
+# use for reference and name/constituent ID changes
+# created: Vince Darcangelo, 8/4/22
+# most recent update: Vince Darcangelo 7/18/25
+# \AIM Measurement - FCQ\R_Code\campus_labs\InstCL_batch.R
+##########################################################################
 
 userid <- 'darcange'
 term_cd <- 2241
 
 # TERM UPDATE: UPDATE THIS BEFORE RUNNING!!!!!
-file_names <- dir(paste0('C:\\Users\\', userid, '\\OneDrive - UCB-O365\\FCQ - AIM_ Measurement\\CampusLabs\\Imports\\', term_cd, '\\Instructor\\'))
-setwd(paste0('C:\\Users\\', userid, '\\OneDrive - UCB-O365\\FCQ - AIM_ Measurement\\CampusLabs\\Imports\\', term_cd, '\\Instructor\\'))
+file_names <- dir(paste0('C:\\Users\\', userid, '\\UCB-O365\\AIM Measurement - FCQ\\CampusLabs\\Imports\\', term_cd, '\\Instructor\\'))
+setwd(file_names)
 instProd <- do.call(rbind,lapply(file_names,read.csv))
 
 #########################################################################
@@ -52,74 +51,74 @@ datasplit <- cbind(instProd, instTerm, instCamp2$Campus)
 colnames(datasplit) <- c('PersonIdentifier', 'SectionIdentifier', 'FirstName', 'LastName', 'Email', 'Role', 'Term', 'Campus')
 
 # import existing log
-instLog <- read.csv(paste0('C:\\Users\\', userid, '\\OneDrive - UCB-O365\\FCQ - AIM_ Measurement\\CampusLabs\\Data_Files\\instLog.csv'))
+instLog <- read.csv(paste0('C:\\Users\\', userid, '\\UCB-O365\\AIM Measurement - FCQ\\CampusLabs\\Data_Files\\instLog.csv'))
 
 # add new semester (datasplit) data to existing log
 updatedLog <- rbind(instLog, datasplit)
 
 # export updated log
-write.csv(updatedLog, paste0('C:\\Users\\', userid, '\\OneDrive - UCB-O365\\FCQ - AIM_ Measurement\\CampusLabs\\Data_Files\\instLog.csv'), row.names = FALSE)
+write.csv(updatedLog, paste0('C:\\Users\\', userid, '\\UCB-O365\\AIM Measurement - FCQ\\CampusLabs\\Data_Files\\instLog.csv'), row.names = FALSE)
 
 #########################################################################
 # PAST SEMESTERS
-file_names <- dir(paste0('C:\\Users\\', userid, '\\OneDrive - UCB-O365\\FCQ - AIM_ Measurement\\CampusLabs\\Imports\\', term_cd, '\\Instructor\\))
-setwd(paste0('C:\\Users\\', userid, '\\OneDrive - UCB-O365\\FCQ - AIM_ Measurement\\CampusLabs\\Imports\\2231\\Instructor\\))
+file_names <- dir(paste0('C:\\Users\\', userid, '\\UCB-O365\\AIM Measurement - FCQ\\CampusLabs\\Imports\\2231\\Instructor\\'))
+setwd(paste0('C:\\Users\\', userid, '\\OneDrive - UCB-O365\\FCQ - AIM_ Measurement\\CampusLabs\\Imports\\2231\\Instructor\\'))
 df2231 <- do.call(rbind,lapply(file_names,read.csv))
 
-file_names <- dir("K:\\IR\\FCQ\\Prod\\2227\\Instructor\\")
-setwd("K:\\IR\\FCQ\\Prod\\2227\\Instructor\\")
+file_names <- dir('K:\\IR\\FCQ\\Prod\\2227\\Instructor\\')
+setwd('K:\\IR\\FCQ\\Prod\\2227\\Instructor\\')
 df2227 <- do.call(rbind,lapply(file_names,read.csv))
 
-file_names <- dir("K:\\IR\\FCQ\\Prod\\2224\\Instructor\\")
-setwd("K:\\IR\\FCQ\\Prod\\2224\\Instructor\\")
+file_names <- dir('K:\\IR\\FCQ\\Prod\\2224\\Instructor\\')
+setwd('K:\\IR\\FCQ\\Prod\\2224\\Instructor\\')
 df2224 <- do.call(rbind,lapply(file_names,read.csv))
 
-file_names <- dir("K:\\IR\\FCQ\\Prod\\2221\\Instructor\\")
-setwd("K:\\IR\\FCQ\\Prod\\2221\\Instructor\\")
+file_names <- dir('K:\\IR\\FCQ\\Prod\\2221\\Instructor\\')
+setwd('K:\\IR\\FCQ\\Prod\\2221\\Instructor\\')
 df2221 <- do.call(rbind,lapply(file_names,read.csv))
 
-file_names <- dir("K:\\IR\\FCQ\\Prod\\2217\\Instructor\\")
-setwd("K:\\IR\\FCQ\\Prod\\2217\\Instructor\\")
+file_names <- dir('K:\\IR\\FCQ\\Prod\\2217\\Instructor\\')
+setwd('K:\\IR\\FCQ\\Prod\\2217\\Instructor\\')
 df2217 <- do.call(rbind,lapply(file_names,read.csv))
 
-file_names <- dir("K:\\IR\\FCQ\\Prod\\2214\\Instructor\\")
-setwd("K:\\IR\\FCQ\\Prod\\2214\\Instructor\\")
+file_names <- dir('K:\\IR\\FCQ\\Prod\\2214\\Instructor\\')
+setwd('K:\\IR\\FCQ\\Prod\\2214\\Instructor\\')
 df2214 <- do.call(rbind,lapply(file_names,read.csv))
 
-file_names <- dir("K:\\IR\\FCQ\\Prod\\2211\\Instructor\\")
-setwd("K:\\IR\\FCQ\\Prod\\2211\\Instructor\\")
+file_names <- dir('K:\\IR\\FCQ\\Prod\\2211\\Instructor\\')
+setwd('K:\\IR\\FCQ\\Prod\\2211\\Instructor\\')
 df2211 <- do.call(rbind,lapply(file_names,read.csv))
 
-file_names <- dir("K:\\IR\\FCQ\\Prod\\2207\\Instructor\\")
-setwd("K:\\IR\\FCQ\\Prod\\2207\\Instructor\\")
+file_names <- dir('K:\\IR\\FCQ\\Prod\\2207\\Instructor\\')
+setwd('K:\\IR\\FCQ\\Prod\\2207\\Instructor\\')
 df2207 <- do.call(rbind,lapply(file_names,read.csv))
 
-file_names <- dir("K:\\IR\\FCQ\\Prod\\2197\\Instructor\\")
-setwd("K:\\IR\\FCQ\\Prod\\2197\\Instructor\\")
+file_names <- dir('K:\\IR\\FCQ\\Prod\\2197\\Instructor\\')
+setwd('K:\\IR\\FCQ\\Prod\\2197\\Instructor\\')
 df2197 <- do.call(rbind,lapply(file_names,read.csv))
 
-file_names <- dir("K:\\IR\\FCQ\\Prod\\2194\\Instructor\\")
-setwd("K:\\IR\\FCQ\\Prod\\2194\\Instructor\\")
+file_names <- dir('K:\\IR\\FCQ\\Prod\\2194\\Instructor\\')
+setwd('K:\\IR\\FCQ\\Prod\\2194\\Instructor\\')
 df2194 <- do.call(rbind,lapply(file_names,read.csv))
 
-file_names <- dir("K:\\IR\\FCQ\\Prod\\2191\\Instructor\\")
-setwd("K:\\IR\\FCQ\\Prod\\2191\\Instructor\\")
+file_names <- dir('K:\\IR\\FCQ\\Prod\\2191\\Instructor\\')
+setwd('K:\\IR\\FCQ\\Prod\\2191\\Instructor\\')
 df2191 <- do.call(rbind,lapply(file_names,read.csv))
 
-file_names <- dir("K:\\IR\\FCQ\\Prod\\2187\\Instructor\\")
-setwd("K:\\IR\\FCQ\\Prod\\2187\\Instructor\\")
+file_names <- dir('K:\\IR\\FCQ\\Prod\\2187\\Instructor\\')
+setwd('K:\\IR\\FCQ\\Prod\\2187\\Instructor\\')
 df2187 <- do.call(rbind,lapply(file_names,read.csv))
 
-file_names <- dir("K:\\IR\\FCQ\\Prod\\2184\\Instructor\\")
-setwd("K:\\IR\\FCQ\\Prod\\2184\\Instructor\\")
+file_names <- dir('K:\\IR\\FCQ\\Prod\\2184\\Instructor\\')
+setwd('K:\\IR\\FCQ\\Prod\\2184\\Instructor\\')
 df2184 <- do.call(rbind,lapply(file_names,read.csv))
 
-file_names <- dir("K:\\IR\\FCQ\\Prod\\2181\\Instructor\\")
-setwd("K:\\IR\\FCQ\\Prod\\2181\\Instructor\\")
+file_names <- dir('K:\\IR\\FCQ\\Prod\\2181\\Instructor\\')
+setwd('K:\\IR\\FCQ\\Prod\\2181\\Instructor\\')
 df2181 <- do.call(rbind,lapply(file_names,read.csv))
 
-file_names <- dir("K:\\IR\\FCQ\\Prod\\2177\\Instructor\\")
-setwd("K:\\IR\\FCQ\\Prod\\2177\\Instructor\\")
+file_names <- dir('K:\\IR\\FCQ\\Prod\\2177\\Instructor\\')
+setwd('K:\\IR\\FCQ\\Prod\\2177\\Instructor\\')
 df2177 <- do.call(rbind,lapply(file_names,read.csv))
 
 #instLog <- rbind(df2177, df2181, df2184, df2187, df2191, df2194, df2197, df2201, df2204, df2207, df2211, df2214, df2217, df2221)
